1e2c05db5ed81d976d3d7d09cf3ab1b0e860852b
ansonChui <chuichitoanson@gmail.com>
2025-04-23 23:10:07 +0800

    backend api integrated Stripe product and price creation

--- backend/api.js
+++ backend/api.js
@@ -11,177 +11,275 @@ update log:
 
 import mongoose from "mongoose";
 import Product from "./models/product.model.js";
+import dotenv from "dotenv";
+import Stripe from "stripe";
+
+// golbal constant
+const CURRENCY = "hkd";
+
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_API_Secret_KEY);
+// console.log(process.env.STRIPE_API_Secret_KEY);
 
 export async function getProducts(req, res) {
-  try {
-    // get all products from mongoDB
-    const products = await Product.find();
-    // return the products
-    res.status(200).send({
-      res: true,
-      message: "Products fetched successfully",
-      data: products,
-    });
-  } catch (error) {
-    console.log(err.message);
-    // return 500 server error
-    res.status(500).json({
-      res: false,
-      message: "unknow Server error",
-    });
-  }
+    try {
+        // get all products from mongoDB
+        const products = await Product.find();
+        // return the products
+        res.status(200).send({
+            res: true,
+            message: "Products fetched successfully",
+            data: products,
+        });
+    } catch (error) {
+        console.log(err.message);
+        // return 500 server error
+        res.status(500).json({
+            res: false,
+            message: "unknow Server error",
+        });
+    }
 }
 
 export async function createProduct(req, res) {
-  // console.log(req);
-  const product = req.body;
-  const newProduct = new Product(product);
+    // console.log(req);
 
-  try {
-    // try to save the product on mongoDB
-    await newProduct.save();
-    res.status(201).send({
-      res: true,
-      message: "Product created successfully",
-      data: newProduct,
-    });
-  } catch (err) {
-    // check if its a validation error, return 400 Bad request
-    if (err instanceof mongoose.Error.ValidationError) {
-      console.log("There is an Validation Error");
+    // get the product from body
+    const product = req.body;
 
-      let msg = [];
+    try {
+        // create a new product in Stripe
+        let stripeProduct, stripePrice;
+        stripeProduct = await stripe.products.create({
+            name: product.name,
+            description: product.description,
+        });
+        console.log(stripeProduct);
 
-      for (const key in err.errors) {
-        msg.push(err.errors[key].message);
-      }
+        // create a new price for that product in Stripe
+        stripePrice = await stripe.prices.create({
+            currency: CURRENCY,
+            product: stripeProduct.id,
+            unit_amount: product.price * 100, // HKD dor convert to HKD cents
+        });
 
-      // return 400 bad request
-      res.status(400).json({
-        res: false,
-        message: msg,
-      });
-    } else {
-      // check if other errors, return 500 server error
-      console.log(err.message);
+        // add stripeProductId and stripePriceId to the product
+        product.stripeProductId = stripeProduct.id;
+        product.stripePriceId = stripePrice.id;
 
-      // return 500 server error
-      res.status(500).json({
-        res: false,
-        message: "unknow Server error",
-      });
+        console.log(product);
+
+        // create MongoDB product document
+        const newProduct = new Product(product);
+        await newProduct.save();
+        res.status(201).send({
+            res: true,
+            message: "Product created successfully",
+            data: newProduct,
+        });
+    } catch (err) {
+        // check if its a validation error, return 400 Bad request
+        if (err instanceof mongoose.Error.ValidationError) {
+            console.log("There is an Validation Error");
+
+            let msg = [];
+
+            for (const key in err.errors) {
+                msg.push(err.errors[key].message);
+            }
+
+            // return 400 bad request
+            res.status(400).json({
+                res: false,
+                message: msg,
+            });
+        } else {
+            // check if other errors, return 500 server error
+            console.log(err.message);
+
+            // return 500 server error
+            res.status(500).json({
+                res: false,
+                message: "unknow Server error",
+            });
+        }
     }
-  }
 }
 
 export async function updateProduct(req, res) {
-  // console.log(req);
-  const updates = req.body;
-  const { id } = req.params;
+    // console.log(req);
+    const updates = req.body;
+    const { id } = req.params;
 
-  console.log(updates, id);
+    console.log(updates, id);
 
-  // check if the id is valid or not, sent 400
-  if (!mongoose.isValidObjectId(id)) {
-    console.log("not valid id, ");
-    res.status(400).json({
-      res: false,
-      message: "invalid id",
-    });
-  }
-
-  try {
-    // try to save the product on mongoDB
-    const updatedProduct = await Product.findByIdAndUpdate(id, updates, {
-      new: true,
-      runValidators: true,
-    });
-    if (updatedProduct === null) {
-      // id not found, return 404
-      console.log("id not found");
-      res.status(404).json({
-        res: false,
-        message: "product Id is not valid / not found",
-      });
-    } else {
-      res.status(201).send({
-        res: true,
-        message: "Product updated successfully",
-        data: updatedProduct,
-      });
+    // check if the id is valid or not, sent 400
+    if (!mongoose.isValidObjectId(id)) {
+        console.log("not valid id, ");
+        res.status(400).json({
+            res: false,
+            message: "invalid id",
+        });
     }
-  } catch (err) {
-    // check if its a validation error, return 400 Bad request
-    if (err instanceof mongoose.Error.ValidationError) {
-      console.log("There is an Validation Error");
 
-      let msg = [];
+    try {
+        // try to save the product on mongoDB
+        const originalProduct = await Product.findById(id);
+        if (originalProduct === null) {
+            // id not found, return 404
+            console.log("id not found");
+            res.status(404).json({
+                res: false,
+                message: "product Id is not valid / not found",
+            });
+        } else {
+            // update the product and price on Stripe
+            const stripeProduct = await stripe.products.update(
+                originalProduct.stripeProductId, {
+                    name: updates.name,
+                    description: updates.description,
+                }
+            );
+            if (updates.price !== originalProduct.price) {
+                // deactivate the old price
+                await stripe.prices.update(originalProduct.stripePriceId, {
+                    active: false,
+                });
+                //crate a new price for the updated product
+                const stripePrice = await stripe.prices.create({
+                    currency: CURRENCY,
+                    product: originalProduct.stripeProductId,
+                    unit_amount: updates.price * 100, // HKD dor convert to HKD cents
+                });
+                updates.stripePriceId = stripePrice.id;
 
-      for (const key in err.errors) {
-        msg.push(err.errors[key].message);
-      }
+                console.log(updates);
+            }
+            //update in the product
+            const updatedProduct = await Product.findByIdAndUpdate(id, updates, {
+                new: true,
+                runValidators: true,
+            });
 
-      // return 400 bad request
-      res.status(400).json({
-        res: false,
-        message: msg,
-      });
-    } else {
-      // check if other errors, return 500 server error
-      console.log(err.message);
+            res.status(201).send({
+                res: true,
+                message: "Product updated successfully",
+                data: updatedProduct,
+            });
+        }
+    } catch (err) {
+        // check if its a validation error, return 400 Bad request
+        if (err instanceof mongoose.Error.ValidationError) {
+            console.log("There is an Validation Error");
 
-      // return 500 server error
-      res.status(500).json({
-        res: false,
-        message: "unknow Server error",
-      });
+            let msg = [];
+
+            for (const key in err.errors) {
+                msg.push(err.errors[key].message);
+            }
+
+            // return 400 bad request
+            res.status(400).json({
+                res: false,
+                message: msg,
+            });
+        } else {
+            // check if other errors, return 500 server error
+            console.log(err.message);
+
+            // return 500 server error
+            res.status(500).json({
+                res: false,
+                message: "unknow Server error",
+            });
+        }
     }
-  }
 }
 
 export async function deleteProduct(req, res) {
-  const { id } = req.params;
-  console.log(id);
+    const { id } = req.params;
+    console.log(id);
 
-  // check if the id is valid or not, sent 404
-  if (!mongoose.isValidObjectId(id)) {
-    res.status(404).json({
-      res: false,
-      message: "product Id is not valid / not found",
-    });
-  }
+    // check if the id is valid or not, sent 404
+    if (!mongoose.isValidObjectId(id)) {
+        res.status(400).json({
+            res: false,
+            message: "invalid id",
+        });
+    }
+    const originalProduct = await Product.findById(id);
+    if (originalProduct === null) {
+        // id not found, return 404
+        console.log("id not found");
+        res.status(404).json({
+            res: false,
+            message: "product Id is not valid / not found",
+        });
+    }
 
-  try {
-    // try to delete the product on mongoDB
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    res.status(200).send({
-      res: true,
-      message: "Product deleted successfully",
-    });
-  } catch (err) {
-    console.log(err.message);
-    // return 500 server error.
-    res.status(500).json({
-      res: false,
-      message: "server error",
-    });
-  }
+    try {
+        //deactivate the prodct on stripe
+        const stripeProduct = await stripe.products.update(
+            originalProduct.stripeProductId, {
+                active: false,
+            }
+        );
+        const stripePrice = await stripe.prices.update(
+            originalProduct.stripePriceId, {
+                active: false,
+            }
+        );
+        // delete the product on mongoDB
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        res.status(200).send({
+            res: true,
+            message: "Product deleted successfully",
+        });
+    } catch (err) {
+        console.log(err.message);
+        // return 500 server error.
+        res.status(500).json({
+            res: false,
+            message: "server error",
+        });
+    }
 }
 
 export async function deleteAllProducts(req, res) {
-  try {
-    // try to delete the product on mongoDB
-    const query = await Product.deleteMany();
-    res.status(200).send({
-      res: true,
-      message: `total ${query["deletedCount"]} Product deleted successfully`,
-    });
-  } catch (err) {
-    console.log(err.message);
-    // return 500 server error
-    res.status(500).json({
-      res: false,
-      message: "unknow Server error",
-    });
-  }
+    try {
+        // get all the active product on Stripe
+        const stripeProducts = await stripe.products.list({
+            active: true,
+        });
+        // loop through the products and deactivate them
+        for (const product of stripeProducts.data) {
+            await stripe.products.update(product.id, {
+                active: false,
+            });
+        }
+        // get all the active prices on Stripe
+        const stripePrices = await stripe.prices.list({
+            active: true,
+        });
+        // loop through the prices and deactivate them
+        for (const price of stripePrices.data) {
+            await stripe.prices.update(price.id, {
+                active: false,
+            });
+        }
+
+        // try to delete the product on mongoDB
+        const query = await Product.deleteMany();
+        res.status(200).send({
+            res: true,
+            message: `total ${query["deletedCount"]} Product deleted successfully`,
+        });
+    } catch (err) {
+        console.log(err.message);
+        // return 500 server error
+        res.status(500).json({
+            res: false,
+            message: "unknow Server error",
+        });
+    }
 }
--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -15,29 +15,37 @@ const NAME_ERROR_MSG = "Product name is required";
 const PRICE_ERROR_MSG = "Product price is required, must greater than 0";
 const CATEGORY_ERROR_MSG = "Product category is required";
 
-const productSchema = new Schema(
-  {
-    // required fields
-    name: {
-      type: String,
-      required: [true, NAME_ERROR_MSG],
-      trim: true,
-    },
-    price: {
-      type: Number,
-      required: [true, PRICE_ERROR_MSG],
-      min: [0, PRICE_ERROR_MSG],
-    },
-    category: {
-      type: String,
-      required: [true, CATEGORY_ERROR_MSG],
-      trim: true,
-    },
-    // optional fields
-    description: { type: String, trim: true },
-    imageUrl: { type: String, trim: true },
-  },
-  { timestamps: true } // automatically add createdAt and updatedAt fields
+const productSchema = new Schema({
+        // required fields
+        name: {
+            type: String,
+            required: [true, NAME_ERROR_MSG],
+            trim: true,
+        },
+        price: {
+            type: Number,
+            required: [true, PRICE_ERROR_MSG],
+            min: [0, PRICE_ERROR_MSG],
+        },
+        category: {
+            type: String,
+            // required: [true, CATEGORY_ERROR_MSG],
+            trim: true,
+        },
+        stripeProductId: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        stripePriceId: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        // optional fields
+        description: { type: String, trim: true },
+        imageUrl: { type: String, trim: true },
+    }, { timestamps: true } // automatically add createdAt and updatedAt fields
 );
 
 const Product = mongoose.model("Product", productSchema);
